a9fff7c2bd75448a369c8f577721e5a0ed5d86c3
Roberto Wesley Overdijk <wesley@ratus.nl>
2012-10-23 16:45:32 +0200

    Added entity binding

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
 return array(
     'router' => array(
@@ -20,10 +13,6 @@ return array(
                     ),
                 ),
             ),
-            // The following is a route to simplify getting started creating
-            // new controllers and actions without needing to create a new
-            // module. Simply drop new controllers in, and you can access them
-            // using the path /application/:controller/:action
             'application' => array(
                 'type'    => 'Literal',
                 'options' => array(
--- module/Motd/src/Motd/Controller/MotdController.php
+++ module/Motd/src/Motd/Controller/MotdController.php
@@ -15,20 +15,51 @@ class MotdController extends AbstractActionController
      */
     protected $motdService;
 
+    /**
+     * @var MotdForm
+     */
+    protected $form;
+
+    /**
+     * @var string The message to show once updated
+     */
+    protected $updateMessage = 'Message of the day saved!';
+
+    /**
+     * The edit action. This page allows you to edit your MOTD!
+     *
+     * @return ViewModel
+     */
     public function editAction()
     {
-        if (($this->getRequest()->isPost())) {
-            $this->getMotdService()->setMotd($this->getRequest()->getPost()->get('motd'));
-            return $this->redirect()->toRoute('motd');
+        $request        = $this->getRequest();
+        $flashMessenger = $this->flashMessenger()->setNamespace(__NAMESPACE__);
+
+        if ($request->isPost()) {
+            $this->getForm()->setData($request->getPost())->isValid();
+            $this->getMotdService()->save();
+            $flashMessenger->addMessage($this->updateMessage);
+
+            return $this->redirect()->toRoute('motd-edit');
         }
 
-        $viewModel       = new ViewModel();
-        $form            = new MotdForm($this->getMotdService()->getEntity(), 'new-motd');
-        $viewModel->form = $form;
+        $viewModel          = new ViewModel();
+        $viewModel->form    = $this->getForm();
+        $viewModel->updated = false;
+
+        if ($flashMessenger->hasMessages()) {
+            $messages = $flashMessenger->getMessages();
+            $viewModel->updated = $messages[0];
+        }
 
         return $viewModel;
     }
 
+    /**
+     * Get the motd service.
+     *
+     * @return  Motd\Service\Motd
+     */
     protected function getMotdService()
     {
         if (null === $this->motdService) {
@@ -36,5 +67,25 @@ class MotdController extends AbstractActionController
         }
 
         return $this->motdService;
+    }
+
+    /**
+     * Set the MotdForm
+     *
+     * @param   MotdForm    $form
+     */
+    public function setForm(MotdForm $form)
+    {
+        $this->form = $form;
+    }
+
+    /**
+     * Get the MOTD form.
+     *
+     * @return MotdForm
+     */
+    protected function getForm()
+    {
+        return $this->form;
     }
 }
